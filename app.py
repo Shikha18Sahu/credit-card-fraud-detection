5403455c66a7b920b4d61e253589aa5b8b9a6096
Shikha Sahu <165999881+Shikha18Sahu@users.noreply.github.com>
2026-06-03 00:12:37 +0530

    Fix indentation for try-except block in load_data

--- app.py
+++ app.py
@@ -142,9 +142,10 @@ def load_data():
     file_id = "1AF4XH5x7ah3DarmjSIpoc6I6IAy0y9ju"
     url = f"https://drive.google.com/uc?export=download&confirm=t&id={file_id}"
     return pd.read_csv(url)
-   try:
-       dataset = load_data()
-   except Exception as e:
+
+try:
+    dataset = load_data()
+except Exception as e:
     st.error(f"Dataset load nahi hua: {e}")
     st.stop()
 
