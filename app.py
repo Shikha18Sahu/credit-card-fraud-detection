573476c0c69b1128055d78ed480cd7dc2d201b2e
Shikha Sahu <165999881+Shikha18Sahu@users.noreply.github.com>
2026-06-02 23:57:24 +0530

    Load dataset in load_data function

--- app.py
+++ app.py
@@ -140,6 +140,7 @@ def load_data():
     file_id = "1AF4XH5x7ah3DarmjSIpoc6I6IAy0y9ju"
     url = f"https://drive.google.com/uc?export=download&confirm=t&id={file_id}"
     return pd.read_csv(url)
+    dataset = load_data()
 
 # ---------------- Sidebar Navigation ----------------
 st.sidebar.title("🧭 Navigation")
